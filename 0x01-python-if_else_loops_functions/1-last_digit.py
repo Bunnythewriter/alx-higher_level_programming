1425a79b3446d5cb43d384184d18846174712ff8
ambunny <ambanialambo@gmail.com>
2024-01-16 17:14:43 +0300

    Task 1 fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+'''This program will assign a random signed number\
+    to the variable number each time it is executed'''
 import random
 number = random.randint(-10000, 10000)
 
@@ -8,6 +10,7 @@ if number > 0:
     if lastDigit > 5:
         print(f'Last digit of {number} is {lastDigit} and is greater than 5')
     elif 0 < lastDigit < 6:
-        print(f'Last digit of {number} is {lastDigit} and is less than 6 and not 0')
+        print(f'Last digit of {number} is {lastDigit}\
+            and is less than 6 and not 0')
 else:
     print(f'Last digit of {number} is {lastDigit} and is 0')
