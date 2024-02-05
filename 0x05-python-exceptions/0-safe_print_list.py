6143b6286103102a712febfb8728a8846d5e8df0
ambunny <ambanialambo@gmail.com>
2024-02-05 16:26:42 +0300

    Task 0.2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,6 @@ def safe_print_list(my_list=[], x=0):
         for elements in range(x):
             print(my_list[elements], end="")
             count += 1
-        print()
-        return count
     except:
         print()
         return count
