cd951ab9b4289686123a80e36eff559869b5001e
ambunny <ambanialambo@gmail.com>
2024-02-05 15:41:05 +0300

    Task 0 and 1 Initial commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    '''A function that prints x elements of a list.'''
+    count = 0
+    try:
+        for elements in range(x):
+            print(my_list[elements], end="")
+            count += 1
+        print()
+        return count
+    except IndexError:
+        print()
+        return count
