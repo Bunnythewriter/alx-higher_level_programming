9b1fdb65ecc0166ce5aa07fb1d29a9b0ffc5ccc6
ambunny <ambanialambo@gmail.com>
2024-02-06 19:34:34 +0300

    Task 1.5 fixed bare except

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     ''' A function that prints an integer with "{:d}".format()'''
     try:
         print('{:d}'.format(value))
-    except:
+    except Exception:
         return False
     return True
