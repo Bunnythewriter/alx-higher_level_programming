b42db24025c32043de9439087177ffc95f9b34c4
ambunny <ambanialambo@gmail.com>
2024-03-27 19:44:08 +0300

    Initial commit

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- a script that removes all records with 
+-- a score <= 5 in the table second_table 
+DROP * FROM second_table
+WHERE score <= 5;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,1 @@
+-- A script that lists all the tables of a database in your MySQL server.
